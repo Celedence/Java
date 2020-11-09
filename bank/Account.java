e9aab4c319d3c23a59114a3e8a9ed6fa9451844f
Brian <brianbb174@protonmail.com>
2020-11-09 14:21:44 -0700

    initial

---
+++ bank/Account.java
@@ -0,0 +1,51 @@
+public class Account {
+
+    private String customerName;
+    private String email;
+    private double balance;
+    private String customerPhoneNumber;
+    private String accountNumber;
+
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+
+}
---
+++ oop/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //write code in here
+        Car porsche = new Car();
+        Car ford = new Car();
+        Car chevrolet = new Car();
+        Car jeep = new Car();
+        Car honda = new Car();
+        Car volvo = new Car();
+        Car toyota = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+        
+    }
+}
