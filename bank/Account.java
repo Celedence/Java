4762daddccf2133ce1807356c49957b8eea64348
Brian <brianbb174@protonmail.com>
2020-11-11 12:19:48 -0700

    final Account

--- bank/Account.java
+++ bank/Account.java
@@ -6,8 +6,44 @@ public class Account {
     private String customerPhoneNumber;
     private String accountNumber;
 
+    public Account() {
+        //default params
+        this("123", 0.00, "Default name", "Default email","default phone")
+    }
+
+
+    public Account(String accountNumber, double balance, String customerName, String email, String customerPhoneNumber){
+
+        System.out.println("Account constructor with params called");
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+
+
+
+    public void deposit(double depositAmount) {
+        this.balance += depositAmount;
+        System.out.println("Deposit amount of " + depositAmount + " made. New balance is " + this.balance);
+    }
+
+    public void withdraw(double withdrawAmount) {
+        if(this.balance - withdrawAmount <=0) {
+            System.out.println(" available is " + this.balance);
+        } else {
+            balance -= withdrawAmount;
+            System.out.println("withdrawel of " + withdrawAmount + " is processed");
+        }
+    }
+
+    
+
 
     public String getCustomerName() {
+        System.out.println("Account owner is " + customerName);
         return customerName;
     }
 
@@ -27,7 +63,7 @@ public class Account {
         return balance;
     }
 
-    public void setBalance(String balance) {
+    public void setBalance(double balance) {
         this.balance = balance;
     }
 
--- bank/Main.java
+++ bank/Main.java
@@ -2,8 +2,19 @@
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] arg) {
 
-        
+
+
+        Account bobsAccount = new Account("23456", 0.00, "Bob Smiling", "bobsemail@bob.com", "480-479-9982");
+    
+        System.out.println(bobsAccount.getAccountNumber());
+        System.out.println(bobsAccount.getBalance());
+        Account mikesAccount = new Account();        
+
+        mikesAccount.setBalance(210.90);
+        System.out.println("this is before print balance");
+        System.out.println(mikesAccount.getBalance());
+    
     }
 }
